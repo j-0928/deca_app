0457d515fa3e63a7cff276256101c1c919f6e4fd
oliverma-california <om86052@yahoo.com>
2023-12-10 10:09:48 -0800

    added qr creation, form completion, reponse viewing functionalities

--- nav/pages/Create.js
+++ nav/pages/Create.js
@@ -2,19 +2,25 @@ import * as React from 'react';
 import { View, TouchableOpacity, Image, Modal, Text, TextInput, Button } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import QRCode from 'react-native-qrcode-svg';
-
 export default function Create({ navigation }) {
     const [modalVisible, setModalVisible] = React.useState(false);
+    const [isVisible, setIsVisible] = React.useState(false);
     const [name, setName] = React.useState('');
     const [date, setDate] = React.useState(''); // New state variable for date
+    const [code, setCode] = React.useState('');
     const [submittedForms, setSubmittedForms] = React.useState([]);
+
+    const toggleResponses = () => {
+        setIsVisible(!isVisible);
+    }
 
     // Function to handle form submission
     const handleSubmit = () => {
-        setSubmittedForms([...submittedForms, { name, date }]);
+        setSubmittedForms([...submittedForms, { name, date, code }]);
         setModalVisible(false);
         setName(''); // Reset the name input
         setDate(''); // Reset the date input
+        setCode('');
     };
 
     return (
@@ -33,7 +39,7 @@ export default function Create({ navigation }) {
                     setModalVisible(!modalVisible);
                 }}
             >
-                <View style={{ marginTop: 50, marginHorizontal: 20, backgroundColor: 'white', padding: 35, alignItems: 'center', elevation: 5 }}>
+                <View style={{ marginTop: 50, marginHorizontal: 20, backgroundColor: 'white', padding: 35, alignItems: 'center', elevation: 5, shadowColor: '#000', shadowOffset: { width: 0, height: 2, }, shadowOpacity: 0.25, shadowRadius: 3.84 }}>
                     <Text style={{ marginBottom: 15, textAlign: 'center' }}>Create Form</Text>
                     <TextInput
                         placeholder="Enter Name"
@@ -47,6 +53,12 @@ export default function Create({ navigation }) {
                         onChangeText={setDate}
                         style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
                     />
+                    <TextInput
+                        placeholder="Enter 6 digit code"
+                        value={code}
+                        onChangeText={setCode}
+                        style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
+                    />
                     <Button title="Create" onPress={handleSubmit} />
                     <Button title="Close" onPress={() => setModalVisible(!modalVisible)} />
                 </View>
@@ -57,7 +69,13 @@ export default function Create({ navigation }) {
                 <View key={index} style={{ margin: 10, padding: 10, borderWidth: 1, borderColor: '#ddd' }}>
                     <Text>Name: {form.name}</Text>
                     <Text>Date: {form.date}</Text>
+                    <Text>Six digit code: {form.code}</Text>
                     <QRCode value={form.name + form.date}/>
+                    <Button title="Show Responses" onPress={toggleResponses} />
+                    <Text>Responses:</Text>
+                    {isVisible && <Text>Name: [name text]]</Text>}
+                    {isVisible && <Text>Grade: [grade text]</Text>}
+                    {isVisible && <Text>Email: [email text]</Text>}
                 </View>
             ))}
         </View>
--- nav/pages/Home.js
+++ nav/pages/Home.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, TextInput, View, StyleSheet, Button, TouchableOpacity, Image, Modal,  } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+
+const FILE_PATH = '../data.json';
 
 export default function Home() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [text, setText] = useState('Not yet scanned')
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [name, setName] = React.useState('');
+  const [grade, setGrade] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  
+   // New state variable for date
+  const [submittedForms, setSubmittedForms] = React.useState([]);
+
+  // Function to handle form submission
+  const handleSubmit = () => {
+      setSubmittedForms([...submittedForms, { name, grade, email}]);
+      setModalVisible(false);
+      setName(''); // Reset the name input
+  };  
 
   const askForCameraPermission = () => {
     (async () => {``
@@ -25,6 +41,10 @@ export default function Home() {
     setText(data)
     console.log('Type: ' + type + '\nData: ' + data)
   };
+
+  const submitCode = ({code}) => {
+    setModalVisible(true);
+  }
 
   // Check permissions and return the screens
   if (hasPermission === null) {
@@ -49,9 +69,43 @@ export default function Home() {
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={{ height: 400, width: 400 }} />
       </View>
+      <TextInput placeholder='or enter the 6 digit code...' style={{ height: 40, marginTop: 10, marginBottom: 10, borderWidth: 1, padding: 10, width: '80%' }} />
+      <Button title="Go" onPress={submitCode} />
       <Text style={styles.maintext}>{text}</Text>
-
       {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+{/* Modal for the registration form */}
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+              setModalVisible(!modalVisible);
+          }}
+      >
+          <View style={{ marginTop: 50, marginHorizontal: 20, backgroundColor: 'white', padding: 35, alignItems: 'center', elevation: 5, shadowColor: '#000', shadowOffset: { width: 0, height: 2, }, shadowOpacity: 0.25, shadowRadius: 3.84 }}>
+              <Text style={{ marginBottom: 15, textAlign: 'center' }}>My Form</Text>
+              <TextInput
+                  placeholder="Enter Name"
+                  value={name}
+                  onChangeText={setName}
+                  style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
+              />
+              <TextInput
+                  placeholder="Enter Grade"
+                  value={grade}
+                  onChangeText={setGrade}
+                  style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
+              />
+              <TextInput
+                  placeholder="Enter Email"
+                  value={email}
+                  onChangeText={setEmail}
+                  style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
+              />
+              <Button title="Submit" onPress={handleSubmit} />
+              <Button title="Close" onPress={() => setModalVisible(!modalVisible)} />
+          </View>
+      </Modal>      
     </View>
   );
 }
