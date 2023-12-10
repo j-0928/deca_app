9e65c4289477c0f68234aca87154f3632ae57a4f
oliverma-california <om86052@yahoo.com>
2023-12-09 17:50:57 -0800

    add qr code generation functionality

--- nav/pages/Create.js
+++ nav/pages/Create.js
@@ -1,10 +1,65 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, TouchableOpacity, Image, Modal, Text, TextInput, Button } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import QRCode from 'react-native-qrcode-svg';
 
-export default function Create({navigation}) {
+export default function Create({ navigation }) {
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const [name, setName] = React.useState('');
+    const [date, setDate] = React.useState(''); // New state variable for date
+    const [submittedForms, setSubmittedForms] = React.useState([]);
+
+    // Function to handle form submission
+    const handleSubmit = () => {
+        setSubmittedForms([...submittedForms, { name, date }]);
+        setModalVisible(false);
+        setName(''); // Reset the name input
+        setDate(''); // Reset the date input
+    };
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{ fontSize: 26, fontWeight: 'bold', }}>This is Create!</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            {/* Button to open the form */}
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <Ionicons name="add-circle-outline" size={64}></Ionicons>
+            </TouchableOpacity>
+
+            {/* Modal for the registration form */}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={{ marginTop: 50, marginHorizontal: 20, backgroundColor: 'white', padding: 35, alignItems: 'center', elevation: 5 }}>
+                    <Text style={{ marginBottom: 15, textAlign: 'center' }}>Create Form</Text>
+                    <TextInput
+                        placeholder="Enter Name"
+                        value={name}
+                        onChangeText={setName}
+                        style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
+                    />
+                    <TextInput
+                        placeholder="Enter Date"
+                        value={date}
+                        onChangeText={setDate}
+                        style={{ height: 40, marginBottom: 10, borderWidth: 1, padding: 10, width: '100%' }}
+                    />
+                    <Button title="Create" onPress={handleSubmit} />
+                    <Button title="Close" onPress={() => setModalVisible(!modalVisible)} />
+                </View>
+            </Modal>
+
+            {/* Displaying submitted forms */}
+            {submittedForms.map((form, index) => (
+                <View key={index} style={{ margin: 10, padding: 10, borderWidth: 1, borderColor: '#ddd' }}>
+                    <Text>Name: {form.name}</Text>
+                    <Text>Date: {form.date}</Text>
+                    <QRCode value={form.name + form.date}/>
+                </View>
+            ))}
         </View>
-    )
+    );
 }
